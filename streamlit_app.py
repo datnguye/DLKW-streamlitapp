780b9ee625581c5ab5cbf194074ce72d891cd56f
Dat Nguyen <datnguyen.it09@gmail.com>
2023-01-30 13:21:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,10 +63,12 @@ st.write(df2[3])
 # Let's create form for complaination
 with st.form(key="form_complaination", clear_on_submit=True):
     st.write("Feed us your apple")
+    c1, c2 = st.columns(2)
     
-    name = st.text_input("Your name")
-    email = st.text_input("Email")
-    
+    with c1:
+        name = st.text_input("Name")
+    with c2:
+        email = st.text_input("Email")
     complaination = st.text_input("What would you like us to improve?")
     
     submit = st.form_submit_button(label="Complains")
