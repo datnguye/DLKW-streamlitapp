99855538373faa6537ed0c61cfc9a2a5ad8b2192
Dat Nguyen <datnguyen.it09@gmail.com>
2023-01-30 12:34:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,20 +36,20 @@ my_catalog = run_query("select color_or_style from catalog_for_website")
 df = pandas.DataFrame(my_catalog)
 
 # temp write the dataframe to the page so I Can see what I am working with
-# streamlit.write(df)
+# st.write(df)
 # put the first column into a list
 color_list = df[0].values.tolist()
 # print(color_list)
 
 # Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+option = st.selectbox('Pick a sweatsuit color or style:', list(color_list))
 
 # We'll build the image caption now, since we can
 product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 # use the option selected to go back and get all the info from the database
 df2 = run_query_scalar("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
-streamlit.image(df2[0], width=400, caption= product_caption)
-streamlit.write('Price: ', df2[1])
-streamlit.write('Sizes Available: ',df2[2])
-streamlit.write(df2[3])
+st.image(df2[0], width=400, caption= product_caption)
+st.write('Price: ', df2[1])
+st.write('Sizes Available: ',df2[2])
+st.write(df2[3])
